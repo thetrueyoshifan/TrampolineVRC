4c34b79a7760c7400ce2ef3df50b0b9f18ddb429
thetrueyoshifan <42860543+thetrueyoshifan@users.noreply.github.com>
2019-10-07 22:22:02 -0500

    Initial commit

---
+++ ConsoleWindow.cs
@@ -0,0 +1,29 @@
+﻿#if (TRAMPOLINE_PC)
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using UnityEngine;
+
+namespace Windows
+{
+    public static class GuiConsole 
+    {
+        [DllImport("kernel32.dll")]
+        private static extern int AllocConsole();
+        [DllImport("user32.dll")]
+        [return: MarshalAs(2)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        private static void ShowConsole() { SetForegroundWindow(GetConsoleWindow()); }
+        public static void CreateConsole()
+        {
+            AllocConsole();
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+            Console.Title = $"{Application.productName} v{Application.version} on {Enum.GetName(typeof(RuntimePlatform), Application.platform).Replace("Player", "")} [VRCModloader]";
+            ShowConsole();
+        }
+    }
+}
+#endif
---
+++ Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+namespace Trampoline
+{
+    public static class Main
+    {
+        public static void Run()
+        {
+#if (TRAMPOLINE_QUEST)
+            byte[] data = File.ReadAllBytes("/sdcard/VRCTools/VRCModLoader.dll");
+#else
+            byte[] data = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "VRCModLoader.dll"));
+#endif
+            Assembly a = Assembly.Load(data);
+            a.GetType("VRCModLoader.Injector").GetMethod("Inject").Invoke(null, null);
+        }
+    }
+}
